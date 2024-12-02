b165c0bb5ce23651adb6a9e2b0a2e582fee57626
Asilbek Asilov <asil.california@gmail.com>
2024-12-01 23:26:32 -0800

    Reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const charsArray = string.split('')
+    let reversedStr = ''
+    for(let i = charsArray.length - 1; i >= 0; i--) {
+        reversedStr += charsArray[i]
+    }
+    return reversedStr
 };
 
 // Do not edit below this line
